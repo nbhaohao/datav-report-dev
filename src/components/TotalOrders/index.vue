b3732c9ef281492e700dfa276beab1e8e9e21b2f
nhsoft.zhangzh <zhangzh@cnlemon.net>
2021-02-23 16:56:47 +0800

    feat:完成迷你面积图

--- src/components/TotalOrders/index.vue
+++ src/components/TotalOrders/index.vue
@@ -1,13 +1,73 @@
 <template>
-  <common-card />
+  <common-card title="累计订单量" value="2,157,420">
+    <template>
+      <div id="total-orders-chart" :style="{ width: '100%', height: '100%' }" />
+    </template>
+    <template #footer>
+      <span>昨日订单量 </span>
+      <span class="emphasis">2,000,000</span>
+    </template>
+  </common-card>
 </template>
 
 <script>
 import commonCardMixins from 'src/mixins/commonCardMixins'
 export default {
   name: 'TotalOrders',
-  mixins: [commonCardMixins]
+  mixins: [commonCardMixins],
+  mounted() {
+    const chartDOM = document.querySelector('#total-orders-chart')
+    const chart = this.$echarts.init(chartDOM)
+    chart.setOption({
+      xAxis: {
+        type: 'category',
+        show: false,
+        boundaryGap: false
+      },
+      yAxis: {
+        show: false
+      },
+      series: [
+        {
+          type: 'line',
+          data: [
+            620,
+            432,
+            220,
+            534,
+            790,
+            430,
+            220,
+            320,
+            532,
+            320,
+            834,
+            690,
+            530,
+            220,
+            620
+          ],
+          areaStyle: {
+            color: 'purple'
+          },
+          smooth: true,
+          lineStyle: {
+            width: 0
+          },
+          itemStyle: {
+            opacity: 0
+          }
+        }
+      ],
+      grid: {
+        left: 0,
+        right: 0,
+        bottom: 0,
+        top: 0
+      }
+    })
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
