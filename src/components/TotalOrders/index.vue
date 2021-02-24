57e75497767ebc16d38a2549f6f163233ad8456e
nbhaohao <nbhaohao@163.com>
2021-02-24 09:39:33 +0800

    feat:使用 v-chart 代替之前的 echarts 用法

--- src/components/TotalOrders/index.vue
+++ src/components/TotalOrders/index.vue
@@ -1,6 +1,7 @@
 <template>
   <common-card title="累计订单量" value="2,157,420">
     <template>
+      <v-chart :option="chartOption" />
       <div id="total-orders-chart" :style="{ width: '100%', height: '100%' }" />
     </template>
     <template #footer>
@@ -15,57 +16,57 @@ import commonCardMixins from 'src/mixins/commonCardMixins'
 export default {
   name: 'TotalOrders',
   mixins: [commonCardMixins],
-  mounted() {
-    const chartDOM = document.querySelector('#total-orders-chart')
-    const chart = this.$echarts.init(chartDOM)
-    chart.setOption({
-      xAxis: {
-        type: 'category',
-        show: false,
-        boundaryGap: false
-      },
-      yAxis: {
-        show: false
-      },
-      series: [
-        {
-          type: 'line',
-          data: [
-            620,
-            432,
-            220,
-            534,
-            790,
-            430,
-            220,
-            320,
-            532,
-            320,
-            834,
-            690,
-            530,
-            220,
-            620
-          ],
-          areaStyle: {
-            color: 'purple'
-          },
-          smooth: true,
-          lineStyle: {
-            width: 0
-          },
-          itemStyle: {
-            opacity: 0
+  computed: {
+    chartOption() {
+      return {
+        xAxis: {
+          type: 'category',
+          show: false,
+          boundaryGap: false
+        },
+        yAxis: {
+          show: false
+        },
+        series: [
+          {
+            type: 'line',
+            data: [
+              620,
+              432,
+              220,
+              534,
+              790,
+              430,
+              220,
+              320,
+              532,
+              320,
+              834,
+              690,
+              530,
+              220,
+              620
+            ],
+            areaStyle: {
+              color: 'purple'
+            },
+            smooth: true,
+            lineStyle: {
+              width: 0
+            },
+            itemStyle: {
+              opacity: 0
+            }
           }
+        ],
+        grid: {
+          left: 0,
+          right: 0,
+          bottom: 0,
+          top: 0
         }
-      ],
-      grid: {
-        left: 0,
-        right: 0,
-        bottom: 0,
-        top: 0
       }
-    })
+    }
   }
 }
 </script>
