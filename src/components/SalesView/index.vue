b0570ba441b6cbc250c900dba104687193867ac2
nbhaohao <nbhaohao@163.com>
2021-02-24 14:54:30 +0800

    feat:完成销售排行榜

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sales-view">
-    <el-card shadow="hover">
+    <el-card shadow="hover" :body-style="{ padding: '0px 0px 20px' }">
       <template #header>
         <div class="menu-wrapper">
           <el-menu
@@ -34,7 +34,21 @@
         </div>
       </template>
       <template>
-        444
+        <div class="sales-view-chart-wrapper">
+          <v-chart :option="chartOption" />
+          <div class="sales-view-list">
+            <div class="sales-view-title">排行榜</div>
+            <div class="list-item-wrapper">
+              <div class="list-item" v-for="item in rankData" :key="item.no">
+                <div :class="[item.no <= 3 ? 'top-no' : '', 'list-item-no']">
+                  {{ item.no }}
+                </div>
+                <div class="list-item-name">{{ item.name }}</div>
+                <div class="list-item-money">{{ item.money }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
     </el-card>
   </div>
@@ -100,12 +114,74 @@ export default {
     return {
       activeIndex: '1',
       radioSelect: '今日',
-      date: null
+      date: null,
+      rankData: [
+        {
+          no: 1,
+          name: '麦当劳',
+          money: '323,234'
+        },
+        {
+          no: 2,
+          name: '麦当劳',
+          money: '323,234'
+        },
+        {
+          no: 3,
+          name: '麦当劳',
+          money: '323,234'
+        },
+        {
+          no: 4,
+          name: '麦当劳',
+          money: '323,234'
+        },
+        {
+          no: 5,
+          name: '麦当劳',
+          money: '323,234'
+        },
+        {
+          no: 6,
+          name: '麦当劳',
+          money: '323,234'
+        },
+        {
+          no: 7,
+          name: '麦当劳',
+          money: '323,234'
+        }
+      ]
     }
   },
   methods: {
     onMenuSelect(index) {
       this.activeIndex = index
+    }
+  },
+  computed: {
+    chartOption() {
+      return {
+        title: {
+          text: '年度销售额',
+          textStyle: {
+            fontSize: 12,
+            color: '#666'
+          }
+        },
+        xAxis: {
+          type: 'category',
+          data: new Array(11).fill('').map((value, index) => `${index + 1}月`)
+        },
+        yAxis: {},
+        series: [
+          {
+            type: 'bar',
+            barWidth: '35%',
+            data: [200, 250, 300, 350, 300, 250, 200, 250, 300, 350, 300, 250]
+          }
+        ]
+      }
     }
   }
 }
@@ -135,6 +211,59 @@ export default {
       justify-content: flex-end;
       .sales-view-date-picker {
         margin-left: 20px;
+      }
+    }
+  }
+  .sales-view-chart-wrapper {
+    display: flex;
+    height: 270px;
+    .echarts {
+      flex: 0 0 70%;
+      width: 70%;
+      height: 100%;
+    }
+    .sales-view-list {
+      flex: 1;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      .sales-view-title {
+        margin-top: 20px;
+        font-size: 12px;
+        color: #666;
+        font-weight: 500;
+      }
+      .list-item-wrapper {
+        margin-top: 15px;
+        .list-item {
+          display: flex;
+          align-items: center;
+          font-size: 12px;
+          height: 20px;
+          padding: 6px 20px 6px 0;
+          .list-item-no {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 20px;
+            height: 20px;
+            color: #333;
+            &.top-no {
+              background: #000;
+              border-radius: 50%;
+              color: #fff;
+              font-weight: 500;
+            }
+          }
+          .list-item-name {
+            margin-left: 10px;
+            color: #333;
+          }
+          .list-item-money {
+            flex: 1;
+            text-align: right;
+          }
+        }
       }
     }
   }
